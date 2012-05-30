fc95d7a7a31ba871e889b046255396851d33c583
8242117@qq.com <8242117@qq.com@1afe9b42-1ce7-97c2-cced-127d38b85e82>
2012-05-30 11:33:30 +0000

    toney

--- trunk/test/main.cpp
+++ trunk/test/main.cpp
@@ -2,10 +2,44 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-int main()
+#include "argc_opt.h"
+
+#include "testlog.h"
+int main(int argc, char *argv[])
 {
 	printf("test is null \n");
-	printf(" 测试程序懂不懂 \n");
+	printf(" test ,do you konw \n");
+
+	testLog();
+	// test getopt
+	char szOptions[] = ":p:l:";
+	Get_Opt cmdOpt;
+	cmdOpt.SetOptions(argc, argv, szOptions);
+	int nOption;
+	while((nOption = cmdOpt.GetOpt()) != EOF)
+	{
+		switch ( nOption ) 
+		{
+		case 'p':		//侦听端口	
+			{
+				int nPort = atoi(cmdOpt.GetCurValue());
+				if(nPort == 0)
+					printf("port is empty \n");
+			}
+			break;
+		case 'l':		//日志等级
+			{
+				int nlog_lv = atoi(cmdOpt.GetCurValue());
+				printf("start log level %d \n",nlog_lv);
+			}
+			break;
+		default:
+			printf("error argv %d \n",nOption);
+			break;
+		}
+	}
+
+
 	return 0;
 }
 
